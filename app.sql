09d928d7e93b1360e9c818916aaf349e3b289427
CAIO M DE OLIVEIRA <melzer.caio@gmail.com>
2015-10-27 07:20:51 -0200

    Update on app.sql

--- app.sql
+++ app.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 26, 2015 at 03:39 AM
+-- Generation Time: Oct 27, 2015 at 10:20 AM
 -- Server version: 5.6.25
 -- PHP Version: 5.6.11
 
@@ -19,6 +19,21 @@ SET time_zone = "+00:00";
 --
 -- Database: `app`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sys_log`
+--
+
+CREATE TABLE IF NOT EXISTS `sys_log` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `id_page` int(11) NOT NULL,
+  `action` text NOT NULL,
+  `data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -93,15 +108,28 @@ CREATE TABLE IF NOT EXISTS `sys_users` (
   `username` varchar(14) DEFAULT NULL,
   `email` varchar(150) NOT NULL,
   `password` varchar(14) DEFAULT NULL,
-  `token` varchar(40) DEFAULT NULL
+  `token` varchar(40) DEFAULT NULL,
+  `status` varchar(1) NOT NULL,
+  `role` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `sys_users`
 --
 
-INSERT INTO `sys_users` (`id`, `username`, `email`, `password`, `token`) VALUES
-(1, 'caiomelzer', 'melzer.caio@gmail.com', 'mewtwo', '1i5m5v4rf1rlik9izkhyflzh2iq9f6r');
+INSERT INTO `sys_users` (`id`, `username`, `email`, `password`, `token`, `status`, `role`) VALUES
+(1, 'caiomelzer', 'melzer.caio@gmail.com', 'mewtwo', 'ob1662vsnbkb7qfrc2mke8iydhg2e29', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sys_users_active`
+--
+
+CREATE TABLE IF NOT EXISTS `sys_users_active` (
+  `id_user` int(11) DEFAULT NULL,
+  `token` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -127,9 +155,43 @@ CREATE TABLE IF NOT EXISTS `sys_users_roles` (
   `id_role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `vw_sys_users`
+--
+CREATE TABLE IF NOT EXISTS `vw_sys_users` (
+`id` int(11)
+,`username` varchar(14)
+,`email` varchar(150)
+,`password` varchar(14)
+,`token` varchar(40)
+,`status` varchar(1)
+,`role` int(11)
+,`name` varchar(25)
+,`id_username` int(11)
+,`fullname` varchar(150)
+,`company` varchar(100)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `vw_sys_users`
+--
+DROP TABLE IF EXISTS `vw_sys_users`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_sys_users` AS select `users`.`id` AS `id`,`users`.`username` AS `username`,`users`.`email` AS `email`,`users`.`password` AS `password`,`users`.`token` AS `token`,`users`.`status` AS `status`,`users`.`role` AS `role`,`roles`.`name` AS `name`,`info`.`id_username` AS `id_username`,`info`.`fullname` AS `fullname`,`info`.`company` AS `company` from ((`sys_users` `users` join `sys_users_info` `info` on((`users`.`id` = `info`.`id_username`))) join `sys_roles` `roles` on((`roles`.`id` = `users`.`role`)));
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `sys_log`
+--
+ALTER TABLE `sys_log`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `sys_pages`
@@ -178,6 +240,11 @@ ALTER TABLE `sys_users_roles`
 --
 
 --
+-- AUTO_INCREMENT for table `sys_log`
+--
+ALTER TABLE `sys_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `sys_payment`
 --
 ALTER TABLE `sys_payment`
@@ -192,6 +259,16 @@ ALTER TABLE `sys_roles`
 --
 ALTER TABLE `sys_users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `sys_signature`
+--
+ALTER TABLE `sys_signature`
+  ADD CONSTRAINT `sys_signature_ibfk_1` FOREIGN KEY (`id`) REFERENCES `sys_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
