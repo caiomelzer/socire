c4a4366cd9125d245d6ca30d82bdbb93b30795ce
CAIO M DE OLIVEIRA <melzer.caio@gmail.com>
2016-02-18 23:55:15 -0200

    Adjust conn file

--- app/core/cone.php
+++ app/core/cone.php
@@ -7,7 +7,9 @@
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	if (!$conn) {
 		$conn = mysqli_connect("localhost", $username, $password, $dbname);
-	    die("Connection failed: " . mysqli_connect_error());
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+		}	  
 	}
  
 ?>
