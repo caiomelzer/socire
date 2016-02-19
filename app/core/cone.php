e08dc98ccf0baca0ee15ed23dba67903283d9dc8
CAIO M DE OLIVEIRA <melzer.caio@gmail.com>
2016-02-18 23:58:28 -0200

    Solve problem with database conn

--- app/core/cone.php
+++ app/core/cone.php
@@ -1,15 +1,17 @@
 <?php
-	$servername = "socire.com:3306";
+	if($_SERVER['HTTP_HOST'] == 'localhost'){
+		$servername = "socire.com:3306";
+	}
+	else{
+		$servername = "localhost";
+	}
 	$username = "socire_adm";
 	$password = "mewtwo";
 	$dbname = "socire_app";
 
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	if (!$conn) {
-		$conn = mysqli_connect("localhost", $username, $password, $dbname);
-		if (!$conn) {
-			die("Connection failed: " . mysqli_connect_error());
-		}	  
+		die("Connection failed: " . mysqli_connect_error());
 	}
  
 ?>
